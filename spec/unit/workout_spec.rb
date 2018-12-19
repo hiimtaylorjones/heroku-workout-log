805f6051b093330512c60ad8054a737368d9b5a1
Taylor Jones <taylor.jones@heroku.com>
2018-12-19 16:36:45 -0500

    adding some css spice to things

--- spec/unit/workout_spec.rb
+++ spec/unit/workout_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Workout, :type => :model do 
 
-    context "is valid" do 
+    it "is valid" do 
         workout = FactoryBot.build(:workout)
         workout.save
         expect(workout.valid?).to eq(true)
