7ef2519f1e274172eea8fe24210871da93d1e8df
Taylor Jones <taylor.jones@heroku.com>
2019-01-24 06:44:12 -0500

    more test coverage

--- spec/functional/movements_controller_spec.rb
+++ spec/functional/movements_controller_spec.rb
@@ -2,10 +2,17 @@ require "rails_helper"
 
 RSpec.describe MovementsController, :type => :controller do 
 
-    it "is valid" do 
-        workout = FactoryBot.create(:workout)
-        movement = FactoryBot.build(:movement, workout: workout)
-        movement.save
-        expect(movement .valid?).to eq(true)
+    describe "GET index" do
+        
+        it "assigns @movements" do
+          movement = FactoryBot.create(:movement)
+          get :index
+          expect(assigns(:movements)).to eq([movement])
+        end
+    
+        it "renders the index template" do
+            get :index
+            expect(response).to render_template("index")
+        end
     end
 end
