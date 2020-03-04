d4b061e69bf6f0b6b13020f67d28aac35074c4e8
Taylor Jones <taylor.jones@salesforce.com>
2020-03-04 17:06:28 -0500

    transferring templates over to ember

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,7 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('home');
+  this.route('home', { path: '/' });
   this.route('workouts', function() {
     this.route('index', { path: '/' } );
     this.route('new', { path: '/new' });
