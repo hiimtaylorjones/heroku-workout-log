147eaed23f05a0e5ee8ccf136a4571c6c0f11164
Taylor Jones <taylor.jones@salesforce.com>
2020-03-04 16:41:59 -0500

    finishing up unit model work

--- frontend/app/models/unit.js
+++ frontend/app/models/unit.js
@@ -3,6 +3,7 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class UnitModel extends Model {
   @belongsTo('workout') workout;
   @belongsTo('movement') movement;
+  
   @attr('string') descriptor;
   @attr('number') amount;
 }
