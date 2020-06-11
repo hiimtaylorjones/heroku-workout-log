0453372880d3204688c6e74363461cc229a127e7
Taylor Jones <taylor.jones@salesforce.com>
2020-06-11 12:48:35 -0400

    workouts controller edits; configuration

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::WorkoutsController < ApplicationController
     workouts = Workout.all
     return render json: Api::V1::WorkoutSerializer.new(workouts).serialized_json
   end
+
+  def show
+    workout = Workout.find(params[:id])
+    return render json: Api::V1::WorkoutSerializer.new(workout).serialized_json
+  end
+
+  def update
+    workout = Workout.find(params[:id])
+    workout.save
+    return render json: Api::V1::WorkoutSerializer.new(workout).serialized_json
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,12 @@ Rails.application.configure do
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
+      origins 'localhost:4200'
+
+     resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+
     end
   end
 end
